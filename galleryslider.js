652f3840a67865404812d3e50d3a1175980b336a
Frank Wang <frank.wangtao@gmail.com>
2014-11-02 17:02:52 -0500

    Fix responsive image orientation

--- galleryslider.js
+++ galleryslider.js
@@ -1,66 +1,4 @@
 (function($) {
-    var RateLimiter = {
-	throttle: function(rawParams, func) {
-	    var defaults = {delay: 200, no_trailing: false};
-	    var params = $.extend(defaults, rawParams);    
-	    var timer;
-	    // If no_trailing false or unspecified
-	    if (!params.no_trailing) {
-		return function() {
-		    var args = arguments;
-		    debounce({delay: params.delay, at_begin: true}, function() {
-			func.apply(this, args);
-		    })();
-		    
-		    if (!timer) {
-			timer = window.setTimeout(function() {
-			    timer = null;
-			    func.apply(this, args);
-			}.bind(this), params.delay);
-		    }
-		}
-	    }
-
-	    // If no_trailing set to true
-	    return function() {
-		var args = arguments;
-		if (!timer) {
-		    func.apply(this, args);
-		    timer = window.setTimeout(function() {
-			timer = null;
-		    }.bind(this), params.delay);
-		}
-	    }
-	},
-
-	debounce: function(rawParams, func) {
-	    var defaults = {delay: 200, at_begin: false};
-	    var params = $.extend(defaults, rawParams);
-	    var timer;
-	    if (!at_begin) {
-		return function() {
-		    window.clearTimeout(timer);
-		    var args = arguments;
-		    timer = window.setTimeout(function() {
-			func.apply(this, args);
-		    }.bind(this), params.delay);
-		};
-	    }
-
-	    return function() {
-		if (!timer) {
-		    func.apply(this, arguments);
-		    timer = window.setTimeout(function() {
-			timer = null;
-		    });
-		} else {
-		    window.clearTimeout(timer);
-		}
-	    };
-	}
-    }
-
-    
     // Given a jQuery collection containing at least 1 image, return true
     // if image wider than gallery box, false otherwise.
     function aspectRatio($image, $container) {
@@ -95,7 +33,7 @@
 		    'left': '50%',
 		    'transform': 'translateX(-50%) translateY(-50%)'
 		};
-		if (aspectRatio($image, $gallery)) {
+		if (aspectRatio($newImage, $gallery)) {
 		    newImageStyle['width'] = '100%';
 		    newImageStyle['height'] = 'auto';
 		} else {
